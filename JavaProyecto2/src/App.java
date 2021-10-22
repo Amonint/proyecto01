99e0df1a27b4e8e80353111a93a2751acd76a4e6
Amon <you@example.com>
2021-10-22 11:01:06 -0500

    version 2

---
+++ JavaProyecto2/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("CLASE 22/10/2021");
+        System.out.println("Plugin");
+        System.out.println("Vs");
+        System.out.println("Holamundo");
+
+    }
+}
