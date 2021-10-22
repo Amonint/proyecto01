b05dac22b6d3fa3b294b0c13eb89fa3b3f745d55
Amon <you@example.com>
2021-10-22 10:52:16 -0500

    JAVAVS

---
+++ proyecto01java/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Generando un miniprograma en Java");
+        System.out.println("Usando Netbeans");
+        System.out.println("Junto");
+        System.out.println("git y git Hub");
+        
+    }
+}
